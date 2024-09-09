2b52f5b400a75b672161e27e8b77eac2f4771b61
IoavM <132418727+IoavM@users.noreply.github.com>
2024-09-09 01:02:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import platform
 st.write("Versión de Python:", platform.python_version())
 
 model = load_model('keras_model.h5')
+model2 = load_model('keras_model2.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes")
@@ -19,10 +20,19 @@ image = Image.open('OIG5.jpg')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
+
+    
+
+
 img_file_buffer = st.camera_input("Toma una Foto")
 
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
+opcion = st.selectbox(   "Modo: ",("Detecta Cara", "Detecta Gesto"))
+
+ if opcion=="Detecta Cara :
+    
+
+   if img_file_buffer is not None:
+    
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
    #To read image file buffer as a PIL Image:
     img = Image.open(img_file_buffer)
@@ -46,5 +56,30 @@ if img_file_buffer is not None:
       st.header('Ioav, con Probabilidad: '+str( prediction[0][1]))
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
+if opcion=="Detecta Gesto" :
+    
+   if img_file_buffer is not None:
+    
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+   #To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
+
+    newsize = (224, 224)
+    img = img.resize(newsize)
+    # To convert PIL Image to numpy array:
+    img_array = np.array(img)
+
+    # Normalize the image
+    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model2.predict(data)
+    print(prediction)
+    if prediction[0][0]>0.5:
+      st.header('Andrea, con Probabilidad: '+str( prediction[0][0]) )
+    if prediction[0][1]>0.5:
+      st.header('Ioav, con Probabilidad: '+str( prediction[0][1]))
 
 
